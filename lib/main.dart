ca2557854b46b8a5067f4b23671509df9e65d7d5
Mateus <mateus.marinho@convicti.com.br>
2023-10-20 19:46:30 -0300

    Partial Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 1,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
   var favorites = <WordPair>[];
@@ -43,7 +80,7 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -54,27 +91,24 @@ class MyHomePage extends StatelessWidget {
         ? icon = Icons.favorite
         : icon = Icons.favorite_border;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            BigCard(pair: pair),
-            SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: appState.toggleFavorite,
-                    icon: Icon(icon),
-                    label: Text("Favoritar")),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    onPressed: appState.getNext,
-                    child: Text("Próxima Palavra")),
-              ],
-            )
-          ]),
-        ),
+    return SafeArea(
+      child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          BigCard(pair: pair),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: appState.toggleFavorite,
+                  icon: Icon(icon),
+                  label: Text("Favoritar")),
+              SizedBox(width: 20),
+              ElevatedButton(
+                  onPressed: appState.getNext, child: Text("Próxima Palavra")),
+            ],
+          )
+        ]),
       ),
     );
   }
